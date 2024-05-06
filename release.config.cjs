b256a27aafa0d826fb5ca9ab7f24cfc1e4453b12
dark-kitt <42038118+dark-kitt@users.noreply.github.com>
2024-05-06 20:25:35 +0200

    feat(no-release): ~ semantic release config

--- release.config.cjs
+++ release.config.cjs
@@ -4,8 +4,37 @@
 module.exports = {
   branches: ["main"],
   plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+        releaseRules: [
+          { scope: "no-release", release: false },
+          { scope: "breaking", release: "major" },
+
+          { type: "feat", release: "minor" },
+          { type: "fix", release: "patch" },
+          { type: "style", release: "patch" },
+
+          { type: "test", release: false }
+        ],
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"]
+        }
+      }
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "angular",
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"]
+        },
+        writerOpts: {
+          commitsSort: ["scope", "subject"]
+        }
+      }
+    ],
     "@semantic-release/npm"
   ]
 };

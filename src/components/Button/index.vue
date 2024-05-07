b765323084128b8549b9ccf689e8f5f1153f9add
dark-kitt <42038118+dark-kitt@users.noreply.github.com>
2024-05-07 19:45:51 +0200

    test(components): + button as test

---
+++ src/components/Button/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <button>{{ text }}</button>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  name?: string;
+  text: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  text: 'button text'
+})
+</script>
+
+<style lang="scss">
+@import './style';
+</style>

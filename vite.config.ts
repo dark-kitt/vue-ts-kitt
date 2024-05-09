768c490a99ae2820c773ae7e56c7e26d869c1e83
dark-kitt <42038118+dark-kitt@users.noreply.github.com>
2024-05-09 18:00:02 +0200

    feat(package): + vite to bundle package

---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+import vue from '@vitejs/plugin-vue';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  cacheDir: '.yarn/cache/.vite',
+  json: {
+    stringify: true
+  },
+  plugins: [
+    vue(),
+    dts({
+      outDir: ['dist', 'src/@types'],
+      cleanVueFileName: true,
+      staticImport: true
+    })
+  ],
+  resolve: {
+    extensions: ['.ts', '.vue', '.json'],
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
+  build: {
+    target: 'ESNext',
+    minify: false,
+    reportCompressedSize: true,
+    lib: {
+      entry: resolve(__dirname, './src/index.ts'),
+      fileName: 'index',
+      formats: ['es', 'cjs']
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+});

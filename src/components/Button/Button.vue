3ebb283c32118b7efbb91601701b936159c811bb
dark-kitt <42038118+dark-kitt@users.noreply.github.com>
2024-05-09 18:03:20 +0200

    fix(components): ~ example button

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button v-bind="HTMLAttributes">{{ text }}</button>
+</template>
+
+<script setup lang="ts">
+import type { ButtonHTMLAttributes } from 'vue';
+import i18n from './i18n.json';
+
+export interface Props {
+  text: string;
+  HTMLAttributes?: ButtonHTMLAttributes;
+}
+
+withDefaults(defineProps<Props>(), {
+  text: i18n.text,
+  HTMLAttributes: () => ({
+    name: i18n.HTMLAttributes.name,
+    type: i18n.HTMLAttributes.type as ButtonHTMLAttributes['type']
+  })
+});
+</script>
+
+<style lang="scss">
+@import './style';
+</style>
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-import Button from '@components/Button';
+import { default as Button } from '@/components/Button';
 
-export { Button };
+export default {
+  Button
+};
